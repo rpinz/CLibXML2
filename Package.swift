52ed74b61e77f60e5676192ff633f5bb293ddbf9
Rene Hexel <r.hexel@griffith.edu.au>
2017-09-24 12:21:01 +1000

    Add pkgConfig and providers

--- Package.swift
+++ Package.swift
@@ -1,3 +1,10 @@
 import PackageDescription 
 
-let package = Package(name: "CLibXML2")
+let package = Package(
+	name: "CLibXML2",
+	pkgConfig: "libxml-2.0",
+	providers: [
+		.Brew("libxml2"),
+		.Apt("libxml2-dev")
+	]
+)
